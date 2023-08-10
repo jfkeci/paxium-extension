0109e08bee54a9b25a01d08f7bb1dd34cdf04ad3
Jakov Filip Sabolicek <ja.lip132@gmail.com>
2023-08-10 12:59:40 +0200

    feat: check in note component

---
+++ src/components/CheckInNote/CheckInNote.jsx
@@ -0,0 +1,45 @@
+import Joi from "joi";
+import { useState } from "react";
+import CheckInNoteEditor from "./CheckInNoteEditor";
+import ValidationInput from "../Shared/ValidationInput";
+
+export default function CheckInNote() {
+  const [checkInNote, setCheckInNote] = useState({
+    template: null,
+    title: "",
+    body: "",
+  });
+
+  return (
+    <div>
+      <ValidationInput
+        config={{
+          type: "text",
+          label: "Title",
+          property: "title",
+          value: checkInNote.title,
+          placeholder: "Note title",
+          onUpdate: (newValue) => {
+            setCheckInNote((prevData) => {
+              console.log("checkInNote", checkInNote);
+              return { ...prevData, title: newValue };
+            });
+          },
+          validationSchema: Joi.string().min(5).max(125).required(),
+        }}
+      />
+
+      <CheckInNoteEditor
+        config={{
+          value: checkInNote.body,
+          onUpdate: (newValue) => {
+            setCheckInNote((prevData) => {
+              console.log("checkInNote", checkInNote);
+              return { ...prevData, body: newValue };
+            });
+          },
+        }}
+      />
+    </div>
+  );
+}
