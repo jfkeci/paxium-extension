066ce8453e40fc614671a3086416c306113172b8
Jakov Filip Sabolicek <ja.lip132@gmail.com>
2023-08-10 13:06:06 +0200

    chore: configure vite build.chunkSizeWarningLimit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), { enableJsx: true }],
+  build: {
+    chunkSizeWarningLimit: 700, // adjust as needed (value is in kB)
+  },
 });
