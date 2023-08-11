59bae1add21cb310333e421c6cc1375db6a0459b
Jakov Filip Sabolicek <ja.lip132@gmail.com>
2023-08-11 16:32:52 +0200

    fix: import instead using require

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import flowbitePlugin from 'flowbite/plugin';
 
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/flowbite/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("flowbite/plugin")({
-      charts: true,
-    }),
-  ],
+  plugins: [flowbitePlugin],
 };
