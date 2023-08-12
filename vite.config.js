3e138e2568921d4a32e3d4a117dbcbcc3501c700
Jakov Filip Sabolicek <ja.lip132@gmail.com>
2023-08-12 09:00:59 +0200

    chore: remove comments from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), { enableJsx: true }],
   build: {
-    chunkSizeWarningLimit: 700, // adjust as needed (value is in kB)
+    chunkSizeWarningLimit: 750,
   },
 });
