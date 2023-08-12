b2fa30a8470ee67a33d065b0ebc812738f25a458
Jakov Filip Sabolicek <ja.lip132@gmail.com>
2023-08-12 09:57:12 +0200

    chore: add flowbite-react to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,10 @@ import flowbitePlugin from 'flowbite/plugin';
 export default {
   content: [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,jsx}',
+    './node_modules/flowbite/*.js',
     './node_modules/flowbite/**/*.js',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
