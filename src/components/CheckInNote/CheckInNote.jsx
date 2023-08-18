24b0dd3a28cf51d09808364faad4ed021f089feb
Jakov Filip Sabolicek <ja.lip132@gmail.com>
2023-08-18 16:35:50 +0200

    feat: add dropdown to check in note component

--- src/components/CheckInNote/CheckInNote.jsx
+++ src/components/CheckInNote/CheckInNote.jsx
@@ -1,9 +1,9 @@
 import Joi from 'joi';
 import { useState } from 'react';
+import { FaFloppyDisk } from 'react-icons/fa6';
+import { Button, Dropdown } from 'flowbite-react';
 import CheckInNoteEditor from './CheckInNoteEditor';
 import ValidationInput from '../Shared/ValidationInput';
-import { FaFloppyDisk } from 'react-icons/fa6';
-import { BsThreeDotsVertical } from 'react-icons/bs';
 
 export default function CheckInNote() {
   const [checkInNote, setCheckInNote] = useState({
@@ -33,21 +33,17 @@ export default function CheckInNote() {
           />
         </div>
         <div>
-          <div className="inline-flex rounded-md shadow-sm mt-2" role="group">
-            <button
-              type="button"
-              className="px-3 py-3 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
-            >
+          <Button.Group className="mt-2">
+            <Button color="gray" size="lg">
               <FaFloppyDisk />
-            </button>
-
-            <button
-              type="button"
-              className="px-3 py-3 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
-            >
-              <BsThreeDotsVertical />
-            </button>
-          </div>
+            </Button>
+            <Dropdown color="gray" size="lg">
+              <Dropdown.Item>Dashboard</Dropdown.Item>
+              <Dropdown.Item>Settings</Dropdown.Item>
+              <Dropdown.Item>Earnings</Dropdown.Item>
+              <Dropdown.Item>Sign out</Dropdown.Item>
+            </Dropdown>
+          </Button.Group>
         </div>
       </div>
 
